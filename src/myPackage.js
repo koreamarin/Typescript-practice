c8c3902c56a807e10cf2769eee381c4c78d54290
koreamarin <awldnjs2@naver.com>
2024-01-23 14:30:43 +0900

    first commit

---
+++ src/myPackage.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {void}
+ */
+function init(config) {
+    return;
+}
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns {number}
+ */
+
+function exit(code) {
+    return code + 1;
+}
